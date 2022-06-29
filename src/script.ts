60112c305b9382823d7aa8dfbc49f67aa7aa8fea
“usuariogit config --global user.name “hernanmf <hernanmartinezf@gmail.com>
2022-06-29 15:33:05 -0300

    armado de script.ts

---
+++ src/script.ts
@@ -0,0 +1,7 @@
+let d = document;
+let iNombre = <HTMLInputElement>d.getElementById("inputNombre");
+let iTelefono = <HTMLInputElement>d.getElementById("inputTelefono");
+let iEmail = <HTMLInputElement>d.getElementById("inputEmail");
+let btnEnviar = <HTMLButtonElement>d.getElementById("botonConsulta");
+
+btnEnviar.addEventListener("click", () => {});
