e1e0a4181a965cb17bacc741e83894b181d253df
“usuariogit config --global user.name “hernanmf <hernanmartinezf@gmail.com>
2022-06-29 22:42:08 -0300

    armando script validador

--- src/script.js
+++ src/script.js
@@ -4,6 +4,15 @@ var iTelefono = d.getElementById("inputTelefono");
 var iEmail = d.getElementById("inputEmail");
 var btnEnviar = d.getElementById("botonConsulta");
 var captcha = Math.round(Math.random() * 1000000);
+var esNumero = new RegExp("^[0-9]*$");
+var esNombre = new RegExp("^[a-zA-Z]+[A-Za-z_ ]+$");
+var esMail = new RegExp("^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 btnEnviar.addEventListener("click", function () {
-    console.log(captcha);
+    if (!esNombre.test(iNombre.value)) {
+        alert(iNombre.value +
+            " no es un nombre válido, revise los datos y vuelva a intentar");
+    }
+    else {
+        console.log(iNombre.value + " es raro");
+    }
 });
--- src/script.ts
+++ src/script.ts
@@ -4,6 +4,19 @@ let iTelefono = <HTMLInputElement>d.getElementById("inputTelefono");
 let iEmail = <HTMLInputElement>d.getElementById("inputEmail");
 let btnEnviar = <HTMLButtonElement>d.getElementById("botonConsulta");
 let captcha = Math.round(Math.random() * 1000000);
+let esNumero = new RegExp("^[0-9]*$");
+let esNombre = new RegExp("^[a-zA-Z]+[A-Za-z_ ]+$");
+let esMail = new RegExp(
+  "^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+);
+
 btnEnviar.addEventListener("click", () => {
-  console.log(captcha);
+  if (!esNombre.test(iNombre.value)) {
+    alert(
+      iNombre.value +
+        "El nombre ingresado no es válido, revise los datos y vuelva a intentar"
+    );
+  } else {
+    console.log(iNombre.value + " es raro");
+  }
 });
